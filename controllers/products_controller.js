88bca21b7983064e6bd6d3a82a07c4fe65f1afa6
souravmalviyavkaps <sourav.malviya@vkaps.com>
2023-05-09 15:32:04 +0530

    some improvisation

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,6 +1,27 @@
 import Product from '../models/product.js';
 import Rating from '../models/rating.js'
 
+export const fetchProductById = async (req, res)=>{
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId).populate({path: 'ratings', populate: {path: 'user'}});
+
+        return res.status(200).json({
+            success: true,
+            message: "Product fetched successfully",
+            data: product
+        })
+    
+
+    } catch (err) {
+        console.log('Error in fetching product by id :', err);
+        return res.status(400).json({
+            success: false,
+            message: 'Error in fetching product : '+err.message
+        })
+    }
+}
+
 export const addProduct = async (req, res)=>{
     try {
         await Product.create(req.body);
@@ -14,7 +35,7 @@ export const addProduct = async (req, res)=>{
         console.log("Error in adding product : ", error);
         return res.status(500).json({
             success: false,
-            message: "Internal server error"
+            message: "Internal server error"+err.message
         })
     }
 }
@@ -25,7 +46,7 @@ export const rateProduct = async (req, res)=>{
         let productId = req.body.productid;
 
         let rating = await Rating.create({
-            user: req.user._id,
+            user: req.body.userid,
             stars: req.body.stars,
             review: req.body.review,
             product: req.body.productid
@@ -33,14 +54,25 @@ export const rateProduct = async (req, res)=>{
 
         //push this rating to products rating array
         let product = await Product.findById(productId);
-        product.ratings.push(rating);
-        product.save();
+        product.ratings.push(rating._id);
+        // product.save();
         
         //change the product avg rating as well 
         
         let ratings = await Rating.find({product: productId});
-        let sumRatings = ratings.reduce((a, b)=> a.stars + b.stars);
-        let avgRating = sumRatings / 5;
+        // console.log(ratings);
+
+        let sumRatings = 0;
+        ratings.map((rating)=>{
+            sumRatings += rating.stars;
+        })
+        console.log(sumRatings)
+        let avgRating;
+        if(ratings.length <= 5){
+             avgRating = sumRatings / 5;
+        }else{
+            avgRating = sumRatings / ratings.length;
+        }
         product.stars = avgRating;
         product.save();
 
@@ -55,7 +87,7 @@ export const rateProduct = async (req, res)=>{
         console.log("Error while rating product : ", err);
         return res.status(500).json({
             success: false,
-            message: "Internal server error"
+            message: "Internal server error : "+err.message
         })
     }
 }
@@ -79,7 +111,7 @@ export const fetchRating = async (req, res)=>{
         console.log("Error while fetching rating of product : ", err);
         return res.status(500).json({
             success: false,
-            message: "Internal server error"
+            message: "Internal server error : "+err.message
         })        
     }
 } 
@@ -102,7 +134,7 @@ export const deleteProduct = async (req, res)=>{
         console.log("Error while deleting product : ", err);
         return res.status(500).json({
             success: false,
-            message: "Could not delete product and associated ratings"
+            message: "Could not delete product and associated ratings : "+err.message
         })
     }
     
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
-import {addProduct, fetchRating, rateProduct, deleteProduct} from '../controllers/products_controller.js';
+import {addProduct, fetchRating, rateProduct, deleteProduct, fetchProductById} from '../controllers/products_controller.js';
 
 router.post('/add-product', addProduct);
+router.get('/fetch-product/:id', fetchProductById);
 router.get('/fetch-rating/:id', fetchRating);
 router.post('/rate-product', rateProduct);
 router.delete('/delete-product/:id', deleteProduct);
-
 
 
 
